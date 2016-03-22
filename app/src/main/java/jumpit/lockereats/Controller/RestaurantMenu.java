0fbf5a54c0206a9fa50cd4e9e189212a9c8c166b
Craig Wilhite <cwilhit@purdue.edu>
2016-03-21 22:05:33 -0400

    iml files

--- app/src/main/java/jumpit/lockereats/Controller/RestaurantMenu.java
+++ app/src/main/java/jumpit/lockereats/Controller/RestaurantMenu.java
@@ -1,10 +1,13 @@
 package jumpit.lockereats.Controller;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
@@ -13,16 +16,24 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.Layout;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 
 import com.bignerdranch.expandablerecyclerview.Model.ParentListItem;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.lockerhops.backend.lockerHopsAPI.LockerHopsAPI;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import jumpit.lockereats.Core.Adapters.RestaurantArrayAdapter;
 import jumpit.lockereats.Core.Adapters.RestaurantExandapleAdapter;
 import jumpit.lockereats.Core.Singleton;
 import jumpit.lockereats.Model.Restaurant;
@@ -127,5 +138,82 @@ public class RestaurantMenu extends AppCompatActivity
         ((View) view.getParent()).findViewById(R.id.menuItemCount);
         TextView countView = (TextView)parent.findViewById(R.id.menuItemCount);
         countView.setText(String.valueOf(newCount));*/
+    }
+
+    private class GetMenuTask extends AsyncTask<Object, Void, List<Restaurant>>
+    {
+        private LockerHopsAPI myApiService = null;
+
+        private ProgressDialog dialog = new ProgressDialog(RestaurantMenu.this);
+
+        @Override
+        protected void onPreExecute() {
+            this.dialog.setMessage("Fetching menu");
+            this.dialog.show();
+        }
+
+        @Override
+        protected List<jumpit.lockereats.Model.Restaurant> doInBackground(Object... params)
+        {
+            if(myApiService == null)
+            {  // Only do this once
+                LockerHopsAPI.Builder builder = new LockerHopsAPI.Builder(AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null)
+                        // options for running against local devappserver
+                        // - 10.0.2.2 is localhost's IP address in Android emulator
+                        // - turn off compression when running against local devappserver
+                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                                abstractGoogleClientRequest.setDisableGZipContent(true);
+                            }
+                        });
+                // end options for devappserver
+
+                myApiService = builder.build();
+            }
+
+            try {
+                List<com.lockerhops.backend.lockerHopsAPI.model.Restaurant> restaurants = myApiService.restaurant().getAllRestaurants().execute().getItems();
+                return jumpit.lockereats.Model.Restaurant.convert(restaurants);
+            } catch (IOException e) {
+                Log.d("Fetch Restaurant", e.getMessage());
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(List<jumpit.lockereats.Model.Restaurant> result) {
+            if (dialog.isShowing()) {
+                dialog.dismiss();
+            }
+
+            if(result != null)
+            {
+                listAdapter = new RestaurantArrayAdapter(RestaurantMenu.this, result);
+                listMenu.setAdapter(listAdapter);
+            }
+            else
+            {
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(RestaurantMenu.this);
+
+                // set title
+
+                alertDialogBuilder.setTitle("An error occured");
+
+                // set dialog message
+                alertDialogBuilder
+                        .setMessage("We were unable to fetch the menu. Close the app and try again later.")
+                        .setPositiveButton("Okay", null);
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // show it
+                alertDialog.show();
+
+            }
+        }
     }
 }
